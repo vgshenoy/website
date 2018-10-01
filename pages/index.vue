caceac3fd1d60fa0dbc6295d79c53bc66b05ee8d
shenoy <vigneshshenoy@gmail.com>
2018-10-01 21:45:19 +0530

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <section class="home section">
+    <div class="container">
+      <div class="title">Design. Tech. Data.</div>
+      <div class="columns">
+        <div class="column is-3">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, optio corporis placeat beatae, quia facilis ipsam praesentium nostrum cumque neque, distinctio at? Vero eaque, consequuntur nihil velit pariatur adipisci eos.</p>
+          <ul>
+            <li>Twitter</li>
+            <li>Youtube</li>
+            <li>Github</li>
+          </ul>
+        </div>
+        <div class="column">
+          <div class="title">Recent Projects</div>
+          <div>
+            projects go here
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
